7fc862396a452fdaaf2c365ee023a64e14babf5e
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-10-12 12:39:32 -0400

    Create conda recipe

---
+++ conda.recipe/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export CMAKE_CONFIG="Release"
+
+mkdir "build_${PKG_NAME}_${CMAKE_CONFIG}"
+pushd "build_${PKG_NAME}_${CMAKE_CONFIG}"
+
+cmake \
+    -G "Unix Makefiles" \
+    -D CMAKE_BUILD_TYPE:STRING="${CMAKE_CONFIG}" \
+    -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+    -D realsense2_DIR:PATH="${REALSENSE2_DIR}" \
+    "${SRC_DIR}/rs_bag2image"
+make
+make install
+
+popd
